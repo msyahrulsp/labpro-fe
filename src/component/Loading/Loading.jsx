b4d7d08e581b6bc40db3a6619e18a53754a35460
msyahrulsp <mssp892@gmail.com>
2022-07-26 10:51:57 +0700

    Add loading component

---
+++ src/component/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import { Flex, Spinner, Text } from '@chakra-ui/react';
+
+export const Loading = () => {
+  return (
+    <Flex mt={8} flexDirection="column" justifyContent="center" alignItems="center">
+      <Spinner speed="0.7s" size="lg" />
+      <Text mt={2}>Loading...</Text>
+    </Flex>
+  )
+}
